1ab06a0674f680b9254206637dda4bcf7f1a0651
aki-com <136059874+aki-com@users.noreply.github.com>
2024-02-06 21:31:11 +0900

    2

---
+++ sctipt.js
@@ -0,0 +1,9 @@
+//inputの中の文字をlistに追加
+function add() {
+    var input = document.getElementById('inputText').value;
+    var list = document.getElementById('list').getElementsByTagName('ul')[0];
+    var li = document.createElement('li');
+    li.textContent = input;
+    ul.appendChild(li);
+    document.getElementById('inputText').value = '';
+}
