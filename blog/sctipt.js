e11e08dee414ea60d2df581e6e33f25e531ec279
aki-com <136059874+aki-com@users.noreply.github.com>
2024-03-17 19:24:14 +0900

    URL取得検査

---
+++ blog/sctipt.js
@@ -0,0 +1,12 @@
+
+var title = document.title;
+document.getElementById("title").innerText = title;
+
+// 現在のページのURLを取得
+var currentPageURL = window.location.href;
+
+// アンカータグのhref属性を現在のページのURLで更新
+document.getElementById("twitter-share").href = "https://twitter.com/share?url=" + encodeURIComponent(currentPageURL) + "&text=" + encodeURIComponent(title) + "&hashtags=ぷなリスト,IT&via=rre_zwihander&related=rre_zwihander";
+document.getElementById("line-share").href = "https://social-plugins.line.me/lineit/share?url=" + encodeURIComponent(currentPageURL);
+document.getElementById("facebook-share").href = "http://www.facebook.com/share.php?u=" + encodeURIComponent(currentPageURL);
+document.getElementById("hatena-share").href = "http://b.hatena.ne.jp/add?mode=confirm&url=" + encodeURIComponent(currentPageURL) + "&title=" + encodeURIComponent(title);
