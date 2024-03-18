2ba8ebfd99823ff2cbee289ead7fbc6b8b73f4e4
aki-com <136059874+aki-com@users.noreply.github.com>
2024-03-18 18:32:54 +0900

    表示だけ成功

--- blog/shere.js
+++ blog/shere.js
@@ -1,3 +1,8 @@
-        fetch("../html_temp/shere.html").then(response => response.text()).then(html => {
-                document.getElementById("shere").innerHTML = html;
-            });
+fetch("/html_temp/shere.html")
+.then(response => response.text())
+.then(html => {
+    document.getElementById("shere").innerHTML = html;
+})
+.catch(error => {
+    console.error("Fetch error:", error);
+});
