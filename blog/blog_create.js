a4df85fc88e013ad50a31182f4eadc7e1e088c3e
aki-com <136059874+aki-com@users.noreply.github.com>
2024-03-19 14:30:50 +0900

    JSファイルを結合

---
+++ blog/blog_create.js
@@ -0,0 +1,23 @@
+Promise.all([
+    fetch("/html_temp/shere.html").then(response => response.text()),
+    fetch("../html_temp/comment.html").then(response => response.text())
+])
+.then(([shereHtml, commentHtml]) => {
+    document.getElementById("field_shere").innerHTML = shereHtml;
+    document.getElementById("field_comment").innerHTML = commentHtml;
+
+    // シェアボタンの設定
+    var title = document.title;
+    var currentPageURL = window.location.href;
+    document.getElementById("twitter-share").href = "https://twitter.com/share?url=" + encodeURIComponent(currentPageURL) + "&text=" + encodeURIComponent(title) + "&hashtags=ぷなリスト,IT&via=rre_zwihander&related=rre_zwihander";
+    document.getElementById("line-share").href = "https://social-plugins.line.me/lineit/share?url=" + encodeURIComponent(currentPageURL);
+    document.getElementById("facebook-share").href = "http://www.facebook.com/share.php?u=" + encodeURIComponent(currentPageURL);
+    document.getElementById("hatena-share").href = "http://b.hatena.ne.jp/add?mode=confirm&url=" + encodeURIComponent(currentPageURL) + "&title=" + encodeURIComponent(title);
+
+    // コメントフォームの設定
+    var url_Part = new URLSearchParams(window.location.search).get('page_name');
+    document.getElementById("comment_API").action = "https://send.pageclip.co/qmYOxqgUuzlkHOgmBK6kcMB75wFZ2B9Y/" + url_Part;
+})
+.catch(error => {
+    console.error("Fetch error:", error);
+});
