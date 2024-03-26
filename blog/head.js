14e75f6fee67307f624a2a97d218914ece998ab5
aki-com <136059874+aki-com@users.noreply.github.com>
2024-03-26 15:26:45 +0900

    Update head.js

--- blog/head.js
+++ blog/head.js
@@ -4,7 +4,11 @@ fetch("../html_temp/head.html")
         // head要素に取得したHTMLを挿入する
         var parser = new DOMParser();
         var head = parser.parseFromString(html, "text/html").querySelector("head");
-        document.head.innerHTML = head.innerHTML;
+        
+        // 新しいhead要素の子要素を一つずつ追加
+        head.childNodes.forEach(node => {
+            document.head.appendChild(node.cloneNode(true));
+        });
     })
     .catch(error => {
         console.error("Fetch error:", error);
