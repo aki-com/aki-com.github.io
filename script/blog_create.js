a8d76e9b02cf7f6bd214445ba41e53e56f584dab
aki-com <136059874+aki-com@users.noreply.github.com>
2024-03-31 12:54:13 +0900

    Update blog_create.js

--- script/blog_create.js
+++ script/blog_create.js
@@ -18,7 +18,7 @@ Promise.all([
     // シェアボタンの設定
     var title = document.title;
     var currentPageURL = window.location.href;
-    document.getElementById("twitter-share").href = "https://twitter.com/share?url=" + encodeURIComponent(currentPageURL) + "&text=" + encodeURIComponent(title) + "&hashtags=ぷなリスト,IT&via=rre_zwihander&related=rre_zwihander";
+    document.getElementById("twitter-share").href = "https://twitter.com/share?url=" + encodeURIComponent(currentPageURL) + "&text=" + encodeURIComponent(title) + "&hashtags=ぷなリスト&via=rre_zwihander&related=rre_zwihander";
     document.getElementById("line-share").href = "https://social-plugins.line.me/lineit/share?url=" + encodeURIComponent(currentPageURL);
     document.getElementById("facebook-share").href = "http://www.facebook.com/share.php?u=" + encodeURIComponent(currentPageURL);
     document.getElementById("hatena-share").href = "http://b.hatena.ne.jp/add?mode=confirm&url=" + encodeURIComponent(currentPageURL) + "&title=" + encodeURIComponent(title);
